26826e1f91f2a3528117dfb7ba86d7b9533721d9
Reham <rehameihab@gmail.com>
2023-08-02 09:40:51 +0100

    Initial Commit

---
+++ NewsPagepractice/NewsPagepracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsPagepracticeApp.swift
+//  NewsPagepractice
+//
+//  Created by scholar on 02/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsPagepracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
